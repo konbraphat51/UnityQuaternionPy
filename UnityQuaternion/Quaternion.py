da1872adb613585ff61e496a6abac7693dbcc4e5
konbraphat51 <konbraphat@gmail.com>
2023-12-19 13:51:57 +0900

    Add: eulerAngles

--- UnityQuaternion/Quaternion.py
+++ UnityQuaternion/Quaternion.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+import math
+
 class Quaternion:
     """
     Quaternion class, immitating UnityEngine.Quaternion
@@ -20,4 +23,63 @@ class Quaternion:
         self.z = z
         self.w = w
         
-    
+    @property
+    def eulerAngles(self) -> tuple[float, float, float]:
+        """
+        Returns or sets the euler angle representation of the rotation in degrees.
+	    The order is Z-rotaion -> X-rotation -> Y-rotation.
+        See: https://docs.unity3d.com/2023.2/Documentation/ScriptReference/Quaternion-eulerAngles.html
+        :rtype: tuple[float, float, float]
+        :return: [x,y,z] in degrees
+        """
+        # based on https://qiita.com/edo_m18/items/5db35b60112e281f840e#unity%E3%81%A7%E5%AE%9F%E8%A1%8C%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B
+        
+        normalized = self.normalized
+        _x = normalized.x
+        _y = normalized.y
+        _z = normalized.z
+        _w = normalized.w
+        
+        sinx = 2 * _y * _z - 2 * _x * _w
+        
+        if (abs(sinx - 1) < 0.01):
+            #sinx == 1 (singularity)
+            x = math.pi / 2
+            y = 0
+            z = math.atan2(2 * _x * _y - 2 * _z * _w, 1 - 2 * _y**2 - 2 * _z**2)
+        elif (abs(sinx + 1) < 0.01):
+            #sinx == -1 (singularity)
+            x = -math.pi / 2
+            y = 0
+            z = math.atan2(2 * _x * _y - 2 * _z * _w, 1 - 2 * _y**2 - 2 * _z**2)
+        else:
+            x = math.asin(-sinx)
+            y = math.atan2(2 * _x * _z + 2 * _y * _w, 1 - 2 * _x**2 - 2 * _y**2)
+            z = math.atan2(2 * _x * _y + 2 * _z * _w, 1 - 2 * _x**2 - 2 * _z**2)
+            
+        # convert to degrees
+        x = math.degrees(x)
+        y = math.degrees(y)
+        z = math.degrees(z)
+        
+        # to [0,360]
+        x = x % 360
+        y = y % 360
+        z = z % 360
+        
+        return (x,y,z)
+        
+    @property
+    def normalized(self) -> Quaternion:
+        """
+        Returns the norm=1 quaternion that represents the same rotation.
+
+        :rtype: Quaternion
+        :return: normalized quaternion
+        """
+        norm = self._norm
+        return Quaternion(self.x/norm, self.y/norm, self.z/norm, self.w/norm)
+        
+    @property
+    def _norm(self) -> float:
+        return (self.x**2 + self.y**2 + self.z**2 + self.w**2)**0.5
