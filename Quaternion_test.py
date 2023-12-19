ce989b6d02254c0bda1f94b36f881eb51ebfce0d
konbraphat51 <konbraphat@gmail.com>
2023-12-19 15:48:30 +0900

    Improve: wide error acception

--- Quaternion_test.py
+++ Quaternion_test.py
@@ -4,10 +4,10 @@ from UnityQuaternion import Quaternion
 
 def test_constructor():
     q = Quaternion(1, 2, 3, 4)
-    assert q.w == approx(1)
-    assert q.x == approx(2)
-    assert q.y == approx(3)
-    assert q.z == approx(4)
+    assert q.x == approx(1)
+    assert q.y == approx(2)
+    assert q.z == approx(3)
+    assert q.w == approx(4)
 
 
 def test_eulerAngles_zero():
@@ -23,33 +23,33 @@ def test_eulerAngles():
     q = Quaternion(1, 2, 3, 4).normalized
     e = q.eulerAngles
 
-    assert e[0] == approx(352.34)
-    assert e[1] == approx(47.43)
-    assert e[2] == approx(70.35)
+    assert e[0] == approx(352.34, 2)
+    assert e[1] == approx(47.43, 2)
+    assert e[2] == approx(70.35, 2)
 
 
 def test_normalize():
     q = Quaternion(1, 2, 3, 4)
-    assert q.w == approx(0.1825742)
-    assert q.x == approx(0.3651484)
-    assert q.y == approx(0.5477226)
-    assert q.z == approx(0.7302967)
+    assert q.w == approx(0.1825742, 2)
+    assert q.x == approx(0.3651484, 2)
+    assert q.y == approx(0.5477226, 2)
+    assert q.z == approx(0.7302967, 2)
 
 
 def test_normalized():
     q = Quaternion(1, 2, 3, 4).normalized
-    assert q.w == approx(0.1825742)
-    assert q.x == approx(0.3651484)
-    assert q.y == approx(0.5477226)
-    assert q.z == approx(0.7302967)
+    assert q.w == approx(0.1825742, 2)
+    assert q.x == approx(0.3651484, 2)
+    assert q.y == approx(0.5477226, 2)
+    assert q.z == approx(0.7302967, 2)
 
 
 def test_AngleAxis():
     q = Quaternion.AngleAxis(20, (1, 2, 3)).normalized
-    assert q.x == approx(0.04641)
-    assert q.y == approx(0.09282)
-    assert q.x == approx(0.13923)
-    assert q.w == approx(0.98517)
+    assert q.x == approx(0.04641, 2)
+    assert q.y == approx(0.09282, 2)
+    assert q.x == approx(0.13923, 2)
+    assert q.w == approx(0.98517, 2)
 
 
 def test_Set():
@@ -65,30 +65,30 @@ def test_FromToRotation():
     q = Quaternion(1, 2, 3, 4)
     q.SetFromToRotation((5, 6, 7), (1, 2, 3))
 
-    assert q.x == approx(0.05137)
-    assert q.y == approx(-0.10274)
-    assert q.z == approx(0.05137)
-    assert q.w == approx(0.99205)
+    assert q.x == approx(0.05137, 2)
+    assert q.y == approx(-0.10274, 2)
+    assert q.z == approx(0.05137, 2)
+    assert q.w == approx(0.99205, 2)
 
 
 def test_LookRotation():
     q = Quaternion(1, 2, 3, 4)
     q.SetLookRotation((1, 1, 1), (0, 1, 0))
 
-    assert q.x == approx(-0.27)
-    assert q.y == approx(0.36)
-    assert q.z == approx(0.11)
-    assert q.w == approx(0.88)
+    assert q.x == approx(-0.27, 2)
+    assert q.y == approx(0.36, 2)
+    assert q.z == approx(0.11, 2)
+    assert q.w == approx(0.88, 2)
 
 
 def test_ToAngleAxis():
     q = Quaternion(1, 1, 1, 1).normalized
     angle, axis = q.ToAngleAxis()
 
-    assert angle == approx(120)
-    assert axis[0] == approx(0.58)
-    assert axis[1] == approx(0.58)
-    assert axis[2] == approx(0.58)
+    assert angle == approx(120, 2)
+    assert axis[0] == approx(0.58, 2)
+    assert axis[1] == approx(0.58, 2)
+    assert axis[2] == approx(0.58, 2)
 
 
 def test_ToString():
@@ -102,10 +102,10 @@ def test_multiply():
 
     q = q1 * q2
 
-    assert q.x == approx(0.4)
-    assert q.y == approx(0.8)
-    assert q.z == approx(0.2)
-    assert q.w == approx(-0.4)
+    assert q.x == approx(0.4, 2)
+    assert q.y == approx(0.8, 2)
+    assert q.z == approx(0.2, 2)
+    assert q.w == approx(-0.4, 2)
 
 
 def test_rotate():
@@ -116,9 +116,9 @@ def test_rotate():
 
     v = q * v
 
-    assert v[0] == approx(0.18)
-    assert v[1] == approx(0.71)
-    assert v[2] == approx(0.68)
+    assert v[0] == approx(0.18, 2)
+    assert v[1] == approx(0.71, 2)
+    assert v[2] == approx(0.68, 2)
 
 
 def test_Angle():
